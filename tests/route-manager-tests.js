29851fc34506c7afd9770fb1aeca374771fddb0a
Jimmy Gleason <jimmy.gleason@akqa.com>
2015-04-27 16:51:29 -0400

    Adds tests for new query params and path name methods

--- tests/route-manager-tests.js
+++ tests/route-manager-tests.js
@@ -58,6 +58,24 @@ describe('Route Manager', function () {
         requireStub.restore();
     });
 
+    it('should return path name from url', function () {
+        var RouteManager = require('route-manager')({config: {}});
+        var url = 'http://my-testable-url.com/my/testable/path';
+        RouteManager.start();
+        var pathName = RouteManager.getPathName(url);
+        assert.equal('/my/testable/path', pathName, 'path name parsed from full url: ' + pathName);
+        RouteManager.stop();
+    });
+
+    it('should return query params from url', function () {
+        var RouteManager = require('route-manager')({config: {}});
+        var url = 'http://my-testable-url.com/my/testable/path/?my=little&tea=pot';
+        RouteManager.start();
+        var queryParams = RouteManager.getQueryParams(url);
+        assert.deepEqual({'my': 'little', 'tea': 'pot'}, queryParams, 'query params parsed from url: ' + JSON.stringify(queryParams));
+        RouteManager.stop();
+    });
+
     it('should fire a url change event when a url is triggered', function () {
         var RouteManager = require('route-manager')({config: {}});
         var url = 'my/testable/url';
