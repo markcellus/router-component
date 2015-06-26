9aa398d3f7296d3b79f1dc36ed596a6e9ca6f76f
Mark <mkay581@gmail.com>
2015-06-26 13:53:17 -0400

    Adding a custom version of module class

---
+++ src/module.js
@@ -0,0 +1,46 @@
+'use strict';
+var BaseModule = require('module-js');
+var Promise = require('promise');
+var _ = require('underscore');
+
+// start element kit
+require('element-kit');
+
+/**
+ * @class Module
+ * @description Base class that represents Modules of an app.
+ * @extends BaseModule
+ */
+var Module = BaseModule.extend({
+
+    /**
+     * Makes a request to get the data for the module.
+     * @param {string} url - The url to fetch data from
+     * @param [options] - ajax options
+     * @returns {*}
+     */
+    fetchData: function (url, options) {
+        return ResourceManager.fetchData(url, options);
+    },
+
+    /**
+     * Gets the css files for the module.
+     * @param cssUrl
+     * @return {Promise}
+     */
+    getStyles: function (cssUrl) {
+        return ResourceManager.loadCss(cssUrl);
+    },
+
+    /**
+     * Gets the html template for the module.
+     * @param templateUrl
+     * @returns {Promise|*}
+     */
+    getTemplate: function (templateUrl) {
+        return ResourceManager.loadTemplate(templateUrl);
+    }
+
+});
+
+module.exports = Module;
