ed4be62639e9f30b4a5ece6568d2d9f67a92434c
Mark <mkay581@gmail.com>
2016-02-10 01:14:29 -0500

    No longer supporting dist files

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,23 +4,6 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         bt: {
-            dist: 'dist',
-            build: {
-                files: {
-                    'dist/router.js': ['src/router.js']
-                },
-                browserifyOptions: {
-                    standalone: 'Router'
-                }
-            },
-            min: {
-                files: {
-                    'dist/router-min.js': ['dist/router.js']
-                }
-            },
-            banner: {
-                files: ['dist/*']
-            },
             tests: {
                 mocha: {
                     src: ['tests/*.js']
