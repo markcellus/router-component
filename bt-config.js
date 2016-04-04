6c8fa234431ee430744ade93b9fbec861d28eb86
Mark <mkay581@gmail.com>
2016-04-04 03:59:42 -0400

    Removing require hack and using latest version of build-tools instead

--- bt-config.js
+++ bt-config.js
@@ -15,12 +15,7 @@ module.exports = {
     },
     tests: {
         mocha: {
-            src: ['tests/*.js'],
-            requires: {
-                // due to the way npm symlinking works, we must use the already-browserified
-                // version of the module to allow for new es features
-                'module-js': './node_modules/module-js/dist/module-min.js'
-            }
+            src: ['tests/*.js']
         }
     }
 
