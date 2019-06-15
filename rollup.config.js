ab344b004f9e71665eb5c76fbe2cb17e7a6e8668
David White <david_white@bnz.co.nz>
2019-06-15 20:23:28 +1200

    Generate commonjs output and set to main

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,16 @@ import resolve from 'rollup-plugin-node-resolve';
 
 export default {
     input: 'src/router-component.ts',
-    output: {
-        format: 'esm',
-        file: 'dist/router-component.js'
-    },
+    output: [
+        {
+            format: 'cjs',
+            file: 'dist/router-component.common.js'
+        },
+        {
+            format: 'esm',
+            file: 'dist/router-component.js'
+        }
+    ],
     plugins: [resolve(), typescript()],
     watch: {
         include: 'src/**'
