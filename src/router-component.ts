106b36de2dc89da005783dcc121b3a53820df135
Mark Kennedy <mark.kennedy@rallyhealth.com>
2019-08-31 12:00:13 -0400

    Support location hashes

--- src/router-component.ts
+++ src/router-component.ts
@@ -81,10 +81,10 @@ export class RouterComponent extends HTMLElement {
         return element;
     }
 
-    show(pathname: string) {
-        if (!pathname) return;
+    show(location: string) {
+        if (!location) return;
         let router;
-
+        const [pathname, hash] = location.split('#');
         const element = this.getRouteElementByPath(pathname);
         if (this.shownPage && this.shownPage.getAttribute('path') !== pathname) {
             this.invalid = true;
@@ -111,6 +111,9 @@ export class RouterComponent extends HTMLElement {
         this.shownPage = element;
         this.appendChild(element);
         this.setupElement(element);
+        if (hash) {
+            window.location.hash = hash;
+        }
 
         this.dispatchEvent(new CustomEvent('route-changed'));
     }
@@ -135,12 +138,12 @@ export class RouterComponent extends HTMLElement {
         const { href } = link;
         if (!href || href.indexOf('mailto:') !== -1) return;
 
-        const location = window.location;
+        const { location } = window;
         const origin = location.origin || location.protocol + '//' + location.host;
         if (href.indexOf(origin) !== 0) return;
         if (link.origin === this.location.origin) {
             e.preventDefault();
-            window.history.pushState({}, document.title, `${link.pathname}${link.search}`);
+            window.history.pushState({}, document.title, `${link.pathname}${link.search}${link.hash}`);
         }
     }
 
