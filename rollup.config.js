411b08d82641f46423521006593cac1f5954fcec
Mark <mark@heyimmark.com>
2020-04-04 21:30:12 -0400

    Upgrade node resolve package

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default {
     input: 'src/router-component.ts',
