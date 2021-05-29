c33458e9a8ea5ed5547c01ad6c759f9b5df8c176
Mark <1446916+markcellus@users.noreply.github.com>
2021-05-29 14:33:10 -0400

    Remove minification from file dists

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,26 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
 
-export default {
-    input: 'src/router-component.ts',
-    output: [
-        {
-            format: 'cjs',
-            file: 'dist/router-component.common.js',
-        },
-        {
+export default [
+    {
+        input: 'src/router-component.ts',
+        output: {
             format: 'esm',
             file: 'dist/router-component.js',
         },
-    ],
-    plugins: [
-        resolve(),
-        typescript(),
-        !process.env.ROLLUP_WATCH &&
-            terser({
-                compress: true,
-                mangle: true,
-            }),
-    ],
-    watch: {
-        include: 'src/**',
+        plugins: [resolve(), typescript()],
+        watch: {
+            include: 'src/**',
+        },
     },
-};
+    {
+        input: 'dist/router-component.js',
+        output: {
+            format: 'cjs',
+            file: 'dist/router-component.common.js',
+        },
+        watch: {
+            include: 'src/**',
+        },
+    },
+];
